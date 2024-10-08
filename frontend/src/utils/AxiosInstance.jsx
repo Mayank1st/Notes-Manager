ad6de9704d72e5a77195dc1e6524704b67226ae9
Mayank Kumar <mayankdas99@gmail.com>
2024-10-08 14:15:35 +0530

    updates_made

--- frontend/src/utils/AxiosInstance.jsx
+++ frontend/src/utils/AxiosInstance.jsx
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000/api/user",
-  timeout: 10000,
+  baseURL: "https://notes-manager-ov5t.onrender.com/api/user",
   headers: {
     "Content-Type": "application/json",
   },
